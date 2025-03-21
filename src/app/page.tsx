5a8f535cb6b5ddf46eb3ab7ac542762cc24bb43f
rhpessoa <rhpessoa29@gmail.com>
2025-03-21 20:41:19 -0300

    Teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Intro from "@/components/Intro";
-import AboutUs from "@/components/AboutUs";
 import Team from "@/components/Team";
 import Footer from "@/components/Footer";
 import Wrapper from "@/components/ui/Wrapper";
 import CompanyServices from "@/components/CompanyServices";
-import Projects from "@/components/Projects";
+
 
 export default function Home() {
   return (
     <Wrapper>
       <Intro />
       <CompanyServices/>
-      {/* <Projects/> */}
-      {/* <AboutUs/> */}
       <Team/>
       <Footer/>
     </Wrapper>
